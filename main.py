7a1b03f2e7a17f264437556be7d9a1ba706fbaeb
tbwcjw <35520616+tbwcjw@users.noreply.github.com>
2025-02-03 15:29:32 -0500

    fixed unmatched f-string

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class TrumpGolfTrack(commands.Bot):
             await self.tree.sync()
             print(f"Logged in as {self.user.name}")
         
-        @self.tree.command(name="track", description=f"{i8ln("track_description")}")
+        @self.tree.command(name="track", description=f"{i8ln('track_description')}")
         async def track(interaction: discord.Interaction):
             log_action("/track", interaction.user)
             await self.show_track(interaction)
@@ -75,16 +75,16 @@ class TrumpGolfTrack(commands.Bot):
             with open(config.CACHE_FILE, 'r') as f:
                 trump_golf_track = Cached(json.load(f))
 
-        embed = discord.Embed(title=f"🏌️ {i8ln("track_title")}", color=config.EMBED_COLOR)
+        embed = discord.Embed(title=f"🏌️ {i8ln('track_title')}", color=config.EMBED_COLOR)
         for key, value in trump_golf_track.to_dict().items():
             #put value on new line if length of value is more than the split threshold
             inline = False if len(str(value)) > config.LINE_LENGTH_SPLIT else True
             embed.add_field(name=f"{key}", value=f"```{value}```", inline=inline)
 
-        embed.add_field(name=f"⌛ {i8ln("days_until")}", value=f"```{countdown()} {i8ln("days")}```")
+        embed.add_field(name=f"⌛ {i8ln('days_until')}", value=f"```{countdown()} {i8ln('days')}```")
         file = discord.File(config.GRAPHS_SAVE_PATH, filename=config.GRAPHS_FILE_NAME)
         embed.set_image(url=f"attachment://{config.GRAPHS_SAVE_PATH}")
-        embed.set_footer(text=f"{i8ln("provided_by")} {config.URL_PATH}, {i8ln("non_affiliate")}")
+        embed.set_footer(text=f"{i8ln('provided_by')} {config.URL_PATH}, {i8ln('non_affiliate')}")
 
         await interaction.followup.send(embed=embed, file=file, ephemeral=config.EPHEMERAL_RESPONSE)
 
